535067807e1e436e8979e8fde4dcc2088a0c23c4
gtokmak <gokhantkmk@gmail.com>
2021-04-16 15:48:08 +0300

    first commit

--- interrupt.py
+++ interrupt.py
@@ -2,35 +2,44 @@ import sys
 import time
 import signal
 import RPi.GPIO as GPIO
-
-IN1 = 13
+from datetime import datetime
+IN1 = 7
 IN2 = 11
-IN3 = 7
+IN3 = 13
 IN4 = 15
+
+def getDate():
+       return datetime.now().strftime('%d-%m-%Y %H:%M:%S:%f')
 
 
 def signal_handler(sig, freme):
     GPIO.cleanup()
+    print('External interrupting application stopped')
     sys.exit()
     
 def button_BOTH_pressed_callback(channel):
-    print('button_BOTH_pressed_callback channel:',channel)
+    if GPIO.input(IN1):
+        print('button_BOTH_pressed_callback channel:' + str(channel) + ' HIGH\tdate:' + getDate())
+    else:
+        print('button_BOTH_pressed_callback channel:' + str(channel) + ' LOW\tdate:' + getDate())
 
 def button_FALLING_pressed_callback(channel):
-    print('button_FALLING_pressed_callback channel:',channel)
+    print('button_FALLING_pressed_callback channel:' + str(channel) + ' \tdate:' + getDate())
     
 def button_RISING_pressed_callback(channel):
-    print('button_RISING_pressed_callback channel:',channel)
+    print('button_RISING_pressed_callback channel:' + str(channel) + ' \tdate:' + getDate())
     
 if __name__ == '__main__':
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(IN1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.setup(IN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.setup(IN3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	
-	GPIO.add_event_detect(IN1, GPIO.BOTH, callback=button_BOTH_pressed_callback, bouncetime=100)
-	GPIO.add_event_detect(IN2, GPIO.FALLING, callback=button_FALLING_pressed_callback, bouncetime=100)
-	GPIO.add_event_detect(IN3, GPIO.RISING, callback=button_RISING_pressed_callback, bouncetime=100)
+    print('Raspberry pi 4 external interrupt application')
+    print('External interrupting application started')
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(IN1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(IN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(IN3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-	signal.signal(signal.SIGINT, signal_handler)
-	signal.pause()
+    GPIO.add_event_detect(IN1, GPIO.BOTH, callback=button_BOTH_pressed_callback, bouncetime=100)
+    GPIO.add_event_detect(IN2, GPIO.FALLING, callback=button_FALLING_pressed_callback, bouncetime=100)
+    GPIO.add_event_detect(IN3, GPIO.RISING, callback=button_RISING_pressed_callback, bouncetime=100)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
